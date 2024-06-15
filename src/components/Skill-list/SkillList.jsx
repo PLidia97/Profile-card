631c0098b94c6cdee7c5f5bc1ec3dc91c8423529
Lidia <lidia_tanase97@yahoo.com>
2024-06-15 23:57:48 +0300

    second commit

--- src/components/Skill-list/SkillList.jsx
+++ src/components/Skill-list/SkillList.jsx
@@ -1,15 +1,49 @@
 import React from "react";
-import Skills from "../Skills/Skills";
+
 import "./SkillList.css";
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "intermediate",
+    color: "#ffff00",
+  },
+  {
+    skill: "JavaScript",
+    level: "beginner",
+    color: "#ffa500",
+  },
+  {
+    skill: "React",
+    level: "beginner",
+    color: "#00ff00",
+  },
+  {
+    skill: "Git & GitHub",
+    level: "beginner",
+    color: "#E84F33",
+  },
+];
 
 const SkillList = () => {
   return (
     <div className="skill-list">
-      <Skills skill="HTML+CSS" emoji="💪" color="yellow" />
-      <Skills skill="JavaScript" emoji="💪" color="orange" />
-      <Skills skill="React" emoji="💪" color="lime" />
+      {skills.map((skill) => (
+        <Skills skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
     </div>
   );
 };
 
 export default SkillList;
+
+const Skills = ({ skill, color, level }) => {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+      </span>
+    </div>
+  );
+};
